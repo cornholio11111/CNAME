662ab96659d6bd12a0783de8abddd211ae6f23df
Tony Conine <conine999990@gmail.com>
2022-11-23 10:33:37 -0500

    Updated 404 Page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById("root"),).render(<App />);
+ReactDOM.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
 // NOTE: DONT TOUCH ANYTHING
 
 // If you want to start measuring performance in your app, pass a function
